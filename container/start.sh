e20337842afcc480bec3e3218316c3dd4c795756
karlamaria100 <karlapantelimon@yahoo.com>
2023-03-19 19:04:48 +0200

    Change container name

--- container/start.sh
+++ container/start.sh
@@ -1,7 +1,7 @@
 composer install --working-dir=addons/mu-plugins/cebola
 cd container
 docker compose up -d
-docker exec container-wp-1 chmod 777 /var/www/html -R
+docker exec container-wpcore-1 chmod 777 /var/www/html -R
 
 docker exec container-wpcore-1 /usr/local/bin/commands/install-missing-exts.sh
 docker exec container-wpcli-1 /usr/local/bin/commands/install-wordpress-core.sh
