57cae262efab9ec50769588775dce8e4cff6e2e3
Zeshan Ahmed <premiumsrapidshares@gmail.com>
2023-03-19 18:12:32 +0100

    extend CommonIssues to test functions

--- addons/mu-plugins/cebola/classes/class-common-issues.php
+++ addons/mu-plugins/cebola/classes/class-common-issues.php
@@ -6,6 +6,9 @@ use PhpParser\NodeVisitorAbstract;
 
 class CommonIssues extends NodeVisitorAbstract {
 	private $return_value = -1;
+	private $wp_funcs = array( 'update_option', 'update_user', 'wp_delete_user' );
+	private $user_meta_funcs = array( 'update_user_meta', 'add_user_meta' );
+	private $php_globals = array( '_GET', '_POST', '_REQUEST', '_SERVER' );
 
 	public function enterNode( Node $node ) {
 		if ( $node instanceof Node\Stmt\If_ ) {
@@ -34,9 +37,20 @@ class CommonIssues extends NodeVisitorAbstract {
 			if ( $node->name instanceof Node\Name ) {
 				$parts = $node->name->parts;
 				$args  = $node->args;
-				if ( in_array( $parts[0], $this->getWpFunctions() ) ) {
+
+				// Check for user meta functions
+				if ( in_array( $parts[0], $this->user_meta_funcs ) ) {
+					$third_arg_val = isset( $args[2] ) ? $this->checkValueIsStringVarOrGlobal( $args[2] ) : 0;
+					// Return importance based on the 3rd argument's value
+					switch ( $third_arg_val ) {
+						case 0: $this->return_value = 0; break;
+						case 1: $this->return_value = 1; break;
+						case 2: $this->return_value = 2; break;
+					}
+				} elseif ( in_array( $parts[0], $this->wp_funcs ) ) {
 					$first_arg_val = isset( $args[0] ) ? $this->checkValueIsStringVarOrGlobal( $args[0] ) : 0;
 					$second_arg_val = isset( $args[1] ) ? $this->checkValueIsStringVarOrGlobal( $args[1] ) : 0;
+
 					if ( $first_arg_val === 0 && $second_arg_val === 1 ) {
 						// if first arg is string/number
 						// and if second argument is variable, return 1.
@@ -106,15 +120,6 @@ class CommonIssues extends NodeVisitorAbstract {
 	}
 
 	/**
-	 * Get WP Functions
-	 *
-	 * @return array
-	 */
-	private function getWpFunctions() {
-		return array( 'update_option', 'update_user', 'wp_delete_user' );
-	}
-
-	/**
 	 * Check Value is String, Variable, or Global
 	 *
 	 * @param object $expr
@@ -123,20 +128,64 @@ class CommonIssues extends NodeVisitorAbstract {
 	private function checkValueIsStringVarOrGlobal( $expr ) {
 		if ( $expr->value instanceof Node\Scalar\String_ ) {
 			// Returning string as 0.
-			return 0;
+			return -1;
 		} elseif ( $expr->value instanceof Node\Scalar\LNumber ) {
 			// returning number as 0.
-			return 0;
+			return -1;
 		} elseif ( $expr->value instanceof Node\Expr\Variable ) {
 			// returning variable as 1.
 			return 1;
 		} elseif ( $expr->value instanceof Node\Expr\ArrayDimFetch ) {
-			if ( in_array( $expr->value->var->name, array( '_GET', '_POST', '_REQUEST', '_SERVER' ) ) ) {
+			// Check if the array variable name is one of PHP globals.
+			if ( in_array( $expr->value->var->name, $this->php_globals ) ) {
+				// then return 2
 				return 2;
-			} else {
+			}
+			// Otherwise return 1.
+			return 1;
+		} elseif ( $expr->value instanceof Node\Expr\FuncCall ) {
+			// Check if value is an instance of 'function'.
+			return $this->checkIfValueIsFunctionSecondLevel( $expr );
+		}
+	}
+
+	/**
+	 * Check Value is String, Variable, or Global
+	 *
+	 * @param object $expr
+	 * @return integer
+	 */
+	private function checkIfValueIsFunctionSecondLevel( $expr ) {
+		// Check if the value is instance of function.
+		if ( $expr->value instanceof Node\Expr\FuncCall ) {
+			$found_global = false;
+			$found_var = false;
+
+			foreach ( $expr->value->args as $arg ) {
+				if ( $arg->value instanceof Node\Expr\ArrayDimFetch ) {
+					if ( in_array( $arg->value->var->name, $this->php_globals ) ) {
+						$found_global = true;
+						break;
+					}
+				} elseif ( $arg->value instanceof Node\Expr\Variable ) {
+					$found_var = true;
+				}
+			}
+
+			// First check if there was a PHP global variable. If yes return highest value 2
+			if ( $found_global ) {
+				return 2;
+			} elseif ( $found_var ) {
+				// If it found a variable instead, return 1.
 				return 1;
+			} else {
+				// Else recursive through the function again.
+				return $this->checkIfValueIsFunctionSecondLevel( $arg );
 			}
 		}
+
+		// If not return 0.
+		return 0;
 	}
 
 	/**
